7cc8fd9b55c10514f70ddb3628f6ddb8caf98efb
Diana K. Navarro <dianakrisnavarro@gmail.com>
2017-12-21 04:32:11 -0500

    we chillin

--- py-pnpoly/pnpoly.py
+++ py-pnpoly/pnpoly.py
@@ -1,11 +1,15 @@
 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+from shapely.geometry import MultiPoint
 import json
 class PolygonGrid():
-    DEFAULT_X = 50
-    DEFAULT_Y = 50
+    DEFAULT_X = 10
+    DEFAULT_Y = 10
 
     #geoJson is just a dictionary
     #geoJson isn't the file, it's when it's loaded in already
+    #we need to initilize the grid before we 
     def __init__(self, geojson, x=DEFAULT_X, y=DEFAULT_Y):
         self.x = x or self.DEFAULT_X
         self.y = y or self.DEFAULT_Y
@@ -20,19 +24,48 @@ class PolygonGrid():
         g = json.load(open(geojson))
         return g
 
-    def init_grid(self):
-        #grid = [[]*self.y for i in range(self.x)] 
-        grid = [[[] for x in range(self.x+1)] for y in range(self.y+1) ]
 
+    def init_grid(self):
+        grid = [[[] for x in range(self.x+1)] for y in range(self.y+1) ]
         for i, polygon in enumerate(self.polygons):
             for point in polygon:
-                x, y = ((point[1]-self.minx) / self.xstep,
-                        (point[0]-self.miny) / self.ystep)
+                #converting a latitude coordinate to the x, y format of a 2D matrix
+                realPoint = (self.maxx - self.minx) - (point[0] - self.minx)
+                x, y = (realPoint / self.xstep,
+                        (point[1]-self.miny) / self.ystep)
                 x = int(x)
                 y = int(y)
                 grid[x][y].append(i)
-        return grid
+        populated_grid = self.populate_grid(grid)
+        return populated_grid
 
+    def populate_grid(self, grid):
+        #populating grid to see which regions are associated with empty boxes in 2D grid
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (len(grid[i][j])) == 0:
+                    x, y = self.findCoordinatesInBox(i,j)
+                    regionList = self.findRegion(x,y)
+                    for k in range(len(regionList)):
+                        grid[i][j].append(regionList[k])
+        return grid
+                    
+    def findCoordinatesInBox(self,i,j):
+        x = i*self.xstep
+        latx = ((self.maxx - self.minx) - x) + self.minx;
+        y = j*self.ystep + self.miny
+        return latx,y
+
+    def findRegion(self, x, y):
+        point = Point(x,y)
+        regionList = []
+        for i in range(len(self.polygons)):
+            polygon = MultiPoint(self.polygons[i]).convex_hull
+            if((polygon.contains(point)) or (polygon.intersects(point))):
+                regionList.append(i)
+        return regionList
+                
+    
     def extract_polygons(self, geojson): #array of all the regions
         return [polygon['geometry']['coordinates'][0] #list of all the coordinates
                 for polygon
@@ -42,7 +75,7 @@ class PolygonGrid():
         minx, miny, maxx, maxy = float('inf'), float('inf'), 0.0, 0.0
         for i, polygon in enumerate(polygons):
             for point in polygon:
-                y, x = point
+                x, y = point
                 maxx = max(maxx, x)
                 maxy = max(maxy, y)
                 minx = min(minx, x)
@@ -65,6 +98,7 @@ class PolygonGrid():
         #candidate polygons are indexes, grabbing from self.grid. 
         
 
+#this pnpoly is after preprocessing because it's using get candidate polygons
 def pnpoly(geojson, points):
     p_grid = PolygonGrid(geojson)
     occurences = [0 * len(p_grid.polygons)] 
@@ -74,6 +108,7 @@ def pnpoly(geojson, points):
         canidate_polygons = p_grid.get_canidate_polygons(x, y)
         #run pn poly on that set of coordinates and the polygons
         for polygon_index in canidate_polygons:
+
             polygon = p_grid.polygons[polygon_index] #polygon is a list of lists, inner lists are long and lat'
 
             in_polygon = False
@@ -89,19 +124,15 @@ if __name__ == '__main__':
     pass
 
 
-"""
+
 
 polygon_grid = PolygonGrid("../data/polygons/estonia_lithuania.geojson")
 
 
-print("steps: :::")
-print(polygon_grid.xstep)
-
-print(polygon_grid.ystep)
 for i, polygon in enumerate(polygon_grid.grid):
     print(i, polygon)
     print("\n")
-"""   
+
 
 
 
